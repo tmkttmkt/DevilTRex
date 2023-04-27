f6748296b14ab1999a9a59fa80a5cdd569feee32
tmkttmkt <kema193491@gmail.com>
2023-04-27 10:23:09 +0900

    Update NewBehaviourScript.cs

--- 実物/コード/NewBehaviourScript.cs
+++ 実物/コード/NewBehaviourScript.cs
@@ -8,14 +8,42 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Debug.Log("aaaa");
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     float speed = 3.0f;
-
+    bool flg_e=true;//押せる状態のときに信
     void Update()
     {
-        // Wキー（前方移動）
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+        }
+        bool input_E = Input.GetKey(KeyCode.E);
+        if (input_E && flg_e)
+        {
+            flg_e = false;
+            if (Cursor.visible)
+            {
+                Debug.Log("kes");
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                Debug.Log("hyouzi");
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+        else if(!input_E && !flg_e) { flg_e = true; }
+            // Wキー（前方移動）
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += speed * transform.forward * Time.deltaTime;
@@ -38,5 +66,7 @@ public class NewBehaviourScript : MonoBehaviour
         {
             transform.position -= speed * transform.right * Time.deltaTime;
         }
+        float mouseX = Input.GetAxis("Mouse X");
+        transform.Rotate(Vector3.up, mouseX * 1000f * Time.deltaTime);
     }
 }
