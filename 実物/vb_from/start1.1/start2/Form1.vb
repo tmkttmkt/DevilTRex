10d19e5ce7eedb55fd94cfbb9caf28167057605d
tmkttmkt <kema193491@gmail.com>
2023-06-01 09:37:16 +0900

    vb_formにexeやった

--- 実物/vb_from/start1.1/start2/Form1.vb
+++ 実物/vb_from/start1.1/start2/Form1.vb
@@ -16,7 +16,8 @@ Public Class Form1
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Try
-            Process.Start("test2.txt")
+            Process.Start("DTR.exe")
+            Application.Exit()
         Catch ex As Exception
             MessageBox.Show("ファイルが開けませんでした:" & ex.Message)
         End Try
