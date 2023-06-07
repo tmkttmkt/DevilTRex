22b82b0a47a57b812bd8f5a33cd454e353f938c5
tmkttmkt <kema193491@gmail.com>
2023-06-07 09:52:25 +0900

    map改造

---
+++ 実物/unity/Goodbye Unity/Assets/Script/rokka.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rokka : MonoBehaviour
+{
+    public GameObject[] rokas;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            foreach (GameObject roka in rokas) { 
+            float dis = Vector3.Distance(this.transform.position, roka.transform.position);
+            Debug.Log("距離 : " + dis);
+        }
+        }
+    }
+}
