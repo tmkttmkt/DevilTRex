8fc41ec656367370678c9f79805fd57ded2223e0
tmkttmkt <kema193491@gmail.com>
2023-07-11 11:51:58 +0900

    7/11save

--- 実物/unity/Goodbye Unity/Assets/Script/teki.cs
+++ 実物/unity/Goodbye Unity/Assets/Script/teki.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 public class teki : MonoBehaviour
 {
+    [SerializeField]Text rast;
     public NavMeshAgent nav;
     [SerializeField]idou_mause target;
     // Start is called before the first frame update
@@ -15,19 +17,26 @@ public class teki : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null )
+        if (target != null)
         {
-            if(!target.tai.flg)
-            nav.destination = target.transform.position;
+            float dis = Vector3.Distance(this.transform.position, target.transform.position);
+            RaycastHit hit;
+            Physics.Raycast(this.transform.position, target.transform.position - this.transform.position, out hit);
+            GameObject hitObject = hit.collider.gameObject;
+            //Debug.Log("距離 : " + dis +"__"+ hitObject);
+            if (target.name == hitObject.name && dis <= 30f) { 
+                if (!target.tai.flg)nav.destination = target.transform.position;
+            }
         }
         else Debug.Log("ngo");
     }
     void OnCollisionEnter(Collision collision)
     {
         //Debug.Log(collision.gameObject);
-        if (collision.gameObject ==target)
+        if (collision.gameObject.name == target.name)
         {
             Debug.Log("Hit");// ログを表示する
+            rast.text = "RIPお前\nGAMEOVERA";
         }
 
     }
--- 実物/unity/Goodbye Unity/Assets/Script/time.cs
+++ 実物/unity/Goodbye Unity/Assets/Script/time.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using System;
+using System.Diagnostics;
+using System.Windows;
 
 public class time : MonoBehaviour
 {
@@ -10,6 +13,7 @@ public class time : MonoBehaviour
     private float timerValue; // タイマーの現在の値
     private bool start = false;
     private Text timerText; // タイマーを表示するテキストオブジェクト
+    [SerializeField] Text rast;
 
     private void Start()
     {
@@ -25,15 +29,30 @@ public class time : MonoBehaviour
             timerText = GetComponent<Text>();
             start = true;
         }
+        timerValue -= Time.deltaTime;
         if (timerValue > 0f)
         {
-            timerValue -= Time.deltaTime; // 経過時間を減算
+             // 経過時間を減算
 
             // タイマーを表示するテキストを更新
             timerText.text = FormatTime(timerValue);
         }
+        else if (timerValue < -10f)
+        {
+            ProcessStartInfo pInfo = new ProcessStartInfo();
+            pInfo.FileName = "notepad";
+            Process.Start(pInfo);
+            //exeじゃなくても閉じるため
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
         else
         {
+            timerText.text = "お前はすでに死んでいるはず";
+            rast.text = "ドロー\nGAMEOVERA";
             // タイマーが0になった場合の処理
             // タイマーが0以下にならないようにする場合は、条件を変更してください
         }
