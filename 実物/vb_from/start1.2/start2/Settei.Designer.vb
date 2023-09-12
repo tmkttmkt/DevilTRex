0cd02b50ffa0130c583f90e5ba5ec67d5a1ae3e8
tmkttmkt <kema193491@gmail.com>
2023-09-12 14:57:12 +0900

    se

--- 実物/vb_from/start1.2/start2/Settei.Designer.vb
+++ 実物/vb_from/start1.2/start2/Settei.Designer.vb
@@ -24,14 +24,14 @@ Partial Class Settei
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.DomainUpDown1 = New System.Windows.Forms.DomainUpDown()
+        Me.DomainUpDown2 = New System.Windows.Forms.DomainUpDown()
         Me.SuspendLayout()
         '
         'Button1
@@ -49,27 +49,6 @@ Partial Class Settei
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(100, 19)
         Me.TextBox1.TabIndex = 1
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(326, 170)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 19)
-        Me.TextBox2.TabIndex = 2
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Location = New System.Drawing.Point(326, 217)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 19)
-        Me.TextBox3.TabIndex = 3
-        '
-        'TextBox4
-        '
-        Me.TextBox4.Location = New System.Drawing.Point(326, 271)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(100, 19)
-        Me.TextBox4.TabIndex = 4
         '
         'Button2
         '
@@ -94,7 +73,7 @@ Partial Class Settei
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Poplar Std", 20.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
-        Me.Label2.Location = New System.Drawing.Point(196, 154)
+        Me.Label2.Location = New System.Drawing.Point(196, 158)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(102, 32)
         Me.Label2.TabIndex = 7
@@ -114,25 +93,48 @@ Partial Class Settei
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Poplar Std", 20.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
-        Me.Label4.Location = New System.Drawing.Point(196, 255)
+        Me.Label4.Location = New System.Drawing.Point(224, 259)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(64, 32)
+        Me.Label4.Size = New System.Drawing.Size(44, 32)
         Me.Label4.TabIndex = 9
-        Me.Label4.Text = "怖さ"
+        Me.Label4.Text = "無"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(326, 271)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 19)
+        Me.TextBox4.TabIndex = 4
+        '
+        'DomainUpDown1
+        '
+        Me.DomainUpDown1.Location = New System.Drawing.Point(326, 171)
+        Me.DomainUpDown1.Name = "DomainUpDown1"
+        Me.DomainUpDown1.Size = New System.Drawing.Size(120, 19)
+        Me.DomainUpDown1.TabIndex = 11
+        Me.DomainUpDown1.Text = "キチガイ"
+        '
+        'DomainUpDown2
+        '
+        Me.DomainUpDown2.Location = New System.Drawing.Point(326, 219)
+        Me.DomainUpDown2.Name = "DomainUpDown2"
+        Me.DomainUpDown2.Size = New System.Drawing.Size(120, 19)
+        Me.DomainUpDown2.TabIndex = 12
+        Me.DomainUpDown2.Text = "キチガイ"
         '
         'Settei
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DomainUpDown2)
+        Me.Controls.Add(Me.DomainUpDown1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.TextBox4)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Settei"
@@ -144,12 +146,12 @@ Partial Class Settei
 
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox4 As TextBox
     Friend WithEvents Button2 As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents DomainUpDown1 As DomainUpDown
+    Friend WithEvents DomainUpDown2 As DomainUpDown
 End Class
