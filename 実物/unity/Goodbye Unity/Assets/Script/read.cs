899c6d68c7ee09fd1113fa48bc467b3c9dc3d831
tmkttmkt <kema193491@gmail.com>
2023-09-12 14:20:11 +0900

    かっった

--- 実物/unity/Goodbye Unity/Assets/Script/read.cs
+++ 実物/unity/Goodbye Unity/Assets/Script/read.cs
@@ -9,30 +9,42 @@ public class read : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StreamReader fail = new StreamReader("test2.txt", Encoding.GetEncoding("Shift_JIS"));
-        string te,key,val;
-        stetting["time"] = 20;
-        Debug.Log("F0");
-        while (true){
-            te = fail.ReadLine();
-            Debug.Log(te);
-            if (te == null) break;
-            val = te.Remove(0, te.IndexOf("=") + 1);
-            key = te.Remove(te.IndexOf("="));
-            stetting[key] = int.Parse(val);
-            Debug.Log(key);
-            Debug.Log(val);
-        }
-        //Debug.Log(stetting);
-        fail.Close();
-
-        using (StreamWriter writer = new StreamWriter("dbg.txt", true))
+        string pa = "test2.txt";
+        if (File.Exists(pa))
         {
-            writer.WriteLine(stetting["time"]);
-            writer.WriteLine("スタート実行");
+            using (StreamReader fail = new StreamReader(pa))
+            {
+                string te, key, val;
+                stetting["time"] = 20;
+                Debug.Log("F0");
+                while (true)
+                {
+                    te = fail.ReadLine();
+                    Debug.Log(te);
+                    if (te == null) break;
+                    val = te.Remove(0, te.IndexOf("=") + 1);
+                    key = te.Remove(te.IndexOf("="));
+                    stetting[key] = int.Parse(val);
+                    Debug.Log(key);
+                    Debug.Log(val);
+                }
+                //Debug.Log(stetting);
+                fail.Close();
+            }
+            using (StreamWriter writer = new StreamWriter("dbg.txt", true))
+            {
+                //writer.WriteLine(stetting["time"]);
+                writer.WriteLine("ファイルある");
+            }
         }
-        stetting["time"] = 600;
-        Debug.Log("F0");
+        else
+        {
+            using (StreamWriter writer = new StreamWriter("dbg.txt", true))
+            {
+                //writer.WriteLine(stetting["time"]);
+                writer.WriteLine("ファイルなし");
+            }
+        }
     }
     // Update is called once per frame
     void Update()
