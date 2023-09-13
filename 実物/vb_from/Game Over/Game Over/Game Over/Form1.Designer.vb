9388746d6b4e553785934a638aa907cdea5e834b
GODAIGOisME <godaigosine@gmail.com>
2023-09-13 09:50:36 +0900

    9/13

---
+++ 実物/vb_from/Game Over/Game Over/Game Over/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(245, 353)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(503, 353)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(180, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(465, 311)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ 実物/vb_from/Game Over/Game Over/Game Over/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+    Private WithEvents gifTimer As New Timer()
+    Private gifFrameIndex As Integer = 0
+    Private gifFrames As List(Of Image) = New List(Of Image)()
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' GIFファイルを読み込み、フレームを分解してリストに保存
+        Dim gif As New System.Drawing.Imaging.FrameDimension(PictureBox1.Image.FrameDimensionsList(0))
+        Dim frameCount As Integer = PictureBox1.Image.GetFrameCount(gif)
+        For i As Integer = 0 To frameCount - 1
+            PictureBox1.Image.SelectActiveFrame(gif, i)
+            gifFrames.Add(New Bitmap(PictureBox1.Image))
+        Next
+
+        ' Timerの設定
+        gifTimer.Interval = 100 ' ミリ秒単位で更新間隔を設定
+        gifTimer.Start()
+    End Sub
+
+    Private Sub gifTimer_Tick(sender As Object, e As EventArgs) Handles gifTimer.Tick
+        ' フレームを表示
+        If gifFrameIndex < gifFrames.Count Then
+            PictureBox1.Image = gifFrames(gifFrameIndex)
+            gifFrameIndex += 1
+        Else
+            ' アニメーションが最後まで再生された場合、停止
+            gifTimer.Stop()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' アニメーションを再生または再開
+        If Not gifTimer.Enabled Then
+            gifTimer.Start()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ' アニメーションを停止
+        gifTimer.Stop()
+    End Sub
+End Class
