f96bafd098c59f1d42c5df44b9e58edd42af2916
GODAIGOisME <godaigosine@gmail.com>
2023-09-20 08:58:51 +0900

    9/20

--- 実物/vb_from/Game Over/Game Over/Form1.Designer.vb
+++ 実物/vb_from/Game Over/Game Over/Form1.Designer.vb
@@ -28,7 +28,9 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ImageList1
@@ -39,12 +41,19 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(355, 351)
+        Me.Button1.BackColor = System.Drawing.Color.DimGray
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Palace Script MT", 18.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button1.Location = New System.Drawing.Point(275, 319)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(251, 49)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Text = "タイトルへ戻る"
+        Me.Button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage
+        Me.Button1.UseVisualStyleBackColor = False
         Me.Button1.Visible = False
         '
         'AxWindowsMediaPlayer1
@@ -53,11 +62,23 @@ Partial Class Form1
         Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(-5, -21)
         Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
         Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
-        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(810, 484)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(810, 493)
         Me.AxWindowsMediaPlayer1.TabIndex = 3
         '
         'Timer1
         '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(-5, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(810, 415)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
         '
         'Form1
         '
@@ -65,9 +86,11 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 415)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.AxWindowsMediaPlayer1)
         Me.Name = "Form1"
         CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -76,4 +99,5 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- 実物/vb_from/Game Over/Game Over/Form1.vb
+++ 実物/vb_from/Game Over/Game Over/Form1.vb
@@ -1,21 +1,30 @@
 ﻿Public Class Form1
     Public count As Integer
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        Dim buttonFlat As FlatButtonAppearance = Button1.FlatAppearance
+        buttonFlat.BorderColor = Color.Gray
         Timer1.Start()
-        AxWindowsMediaPlayer1.URL = "Game Over.mp4"
+        AxWindowsMediaPlayer1.URL = "terano3.mp4"
         AxWindowsMediaPlayer1.Ctlcontrols.play()
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
+        Try
+            Process.Start("start2.exe")
+            Application.Exit()
+        Catch ex As Exception
+            MessageBox.Show("ファイルが開けませんでした:" & ex.Message)
+        End Try
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         count += 1
         If count >= 48 Then
             AxWindowsMediaPlayer1.Ctlcontrols.pause()
-            If count = 60 Then
+            If count = 70 Then
                 Timer1.Stop()
+                PictureBox1.Visible = True
                 Button1.Visible = True
             End If
         End If
