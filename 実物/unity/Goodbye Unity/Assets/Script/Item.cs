55ec95d41415c81efe37acb39d31d18a92d3fc45
tmkttmkt <kema193491@gmail.com>
2023-10-12 12:31:08 +0900

    sav

--- 実物/unity/Goodbye Unity/Assets/Script/Item.cs
+++ 実物/unity/Goodbye Unity/Assets/Script/Item.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
     [SerializeField] ata obj;
     // Start is called before the first frame update
     [SerializeField] GameObject item;
+    [SerializeField] Text[] texts=new Text[3];
     void Start()
     {
         item.gameObject.SetActive(flg_item);
@@ -18,6 +20,11 @@ public class Item : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))//GetKeyDownで押したときに1回動作
         {
+            int n = 0;
+            foreach (Aitem ai in obj.items)
+            {
+                texts[n].text = ai.exem;
+            }
             flg_item = !flg_item;
             item.gameObject.SetActive(flg_item);
         }
