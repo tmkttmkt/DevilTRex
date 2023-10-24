2ccfa272ad61eb0be77d98de59dd4bd71a8b0381
GODAIGOisME <godaigosine@gmail.com>
2023-10-24 15:26:45 +0900

    10/24

--- 実物/vb_from/start1.2/start2/Settei.vb
+++ 実物/vb_from/start1.2/start2/Settei.vb
@@ -21,22 +21,30 @@ Public Class Settei
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Using writer As New StreamWriter("test2.txt", False, Encoding.GetEncoding("Shift_JIS"))
-            writer.WriteLine("teki=" + TextBox1.Text)
-            If DomainUpDown1.Text = ss(0) Then
-                writer.WriteLine("time=" + "300")
-            ElseIf DomainUpDown1.Text = ss(1) Then
-                writer.WriteLine("time=" + "180")
-            ElseIf DomainUpDown1.Text = ss(2) Then
-                writer.WriteLine("time=" + "30")
+            If File.Exists("test2.txt") Then
+                writer.WriteLine("teki=" + TextBox1.Text)
+                If DomainUpDown1.Text = ss(0) Then
+                    writer.WriteLine("time=" + "300")
+                ElseIf DomainUpDown1.Text = ss(1) Then
+                    writer.WriteLine("time=" + "180")
+                ElseIf DomainUpDown1.Text = ss(2) Then
+                    writer.WriteLine("time=" + "30")
+                End If
+                If DomainUpDown2.Text = ss(0) Then
+                    writer.WriteLine("speed=" + "4")
+                ElseIf DomainUpDown2.Text = ss(1) Then
+                    writer.WriteLine("speed=" + "3")
+                ElseIf DomainUpDown2.Text = ss(2) Then
+                    writer.WriteLine("speed=" + "2")
+                End If
+                writer.WriteLine("アイテムの数=" + TextBox4.Text)
+                Label5.Text = "書き込みに成功しました。"
+                Label6.Text = ""
+            Else
+                Label5.Text = "申し訳ございません。"
+                Label6.Text = "書き込みに失敗しました。"
             End If
-            If DomainUpDown2.Text = ss(0) Then
-                writer.WriteLine("speed=" + "4")
-            ElseIf DomainUpDown2.Text = ss(1) Then
-                writer.WriteLine("speed=" + "3")
-            ElseIf DomainUpDown2.Text = ss(2) Then
-                writer.WriteLine("speed=" + "2")
-            End If
-            writer.WriteLine("アイテムの数=" + TextBox4.Text)
+
         End Using
     End Sub
 
@@ -114,6 +122,8 @@ Public Class Settei
         ElseIf count >= 6 Then
             count = 0
             PictureBox1.Image = Image.FromFile("U:\github\tet\実物\画像\設定画面.png")
+            Label5.Text = "文字にカーソルを合わせると"
+            Label6.Text = "説明文が出ます。"
             Me.Hide()
             Form1.Show()
             Timer1.Enabled = False
