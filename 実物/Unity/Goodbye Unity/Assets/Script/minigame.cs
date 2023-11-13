0cce1ada62f7da99bf7a9fc2c78532348848b296
GODAIGOisME <godaigosine@gmail.com>
2023-11-13 15:27:15 +0900

    11/13

--- 実物/Unity/Goodbye Unity/Assets/Script/minigame.cs
+++ 実物/Unity/Goodbye Unity/Assets/Script/minigame.cs
@@ -7,49 +7,48 @@ public class minigame : MonoBehaviour
     public float detectionRadius = 10f; // プレイヤーを検出する半径
     public Color flashColor = Color.white; // 白い画面の色
     public float flashDuration = 1f; // 白い画面が表示される時間
-
+    public GameObject plyerrr;
     private bool isPlayerNear = false;
-
+    public Canvas canvas;
     void Update()
     {
         // プレイヤーの近くにいるかどうかを検出
         isPlayerNear = IsPlayerNear();
-
+        Debug.Log(isPlayerNear);
         // エンターキーが押されたら白い画面を表示
-        if (isPlayerNear && Input.GetKeyDown(KeyCode.Return))
+        if (isPlayerNear == true)
         {
-            StartCoroutine(FlashWhiteScreen());
+           FlashWhiteScreen();
         }
     }
 
     bool IsPlayerNear()
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
 
         // プレイヤーオブジェクトが存在しない場合はプレイヤーは近くにいないと判定
-        if (player == null)
+        float disY = Mathf.Abs(plyerrr.transform.position.y - this.transform.position.y);
+        if (disY <= 3F)
         {
-            //ちなみにtagは使ってないから多分一生このif文起動しない。変数に入れたオブジェで判定してる。
-            //これで黄色出せるんだー
-            Debug.LogWarning("プレイヤーオブジェクトが見つかりませんでした。");
-            return false;
+            // プレイヤーとの距離が検出半径以内かどうかを判定
+            float distanceToPlayer = Vector3.Distance(transform.position, plyerrr.transform.position);
+            return distanceToPlayer <= detectionRadius;
         }
-
-        // プレイヤーとの距離が検出半径以内かどうかを判定
-        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
-        return distanceToPlayer <= detectionRadius;
+        return false;
         // "Player"タグを持つオブジェクトを検索
     }
 
-    System.Collections.IEnumerator FlashWhiteScreen()
+    void FlashWhiteScreen()
     {
         // 白い画面を表示
-        Camera.main.backgroundColor = flashColor;
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
 
-        // 一定時間待ってから元の色に戻す
-        yield return new WaitForSeconds(flashDuration);
+        // Image（四角形）を作成してCanvasに追加
+        GameObject squareObject = new GameObject("WhiteSquare");
+        squareObject.transform.SetParent(canvasObject.transform);
+        RectTransform rectTransform = squareObject.AddComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(200, 200); // 四角形のサイズを設定
 
-        // 元の色に戻す
-        Camera.main.backgroundColor = Color.black; // または適切な背景色に変更
+        Image image = squareObject.AddComponent<Image>();
+        image.color = Color.white; // 白い色を設定
     }
 }
