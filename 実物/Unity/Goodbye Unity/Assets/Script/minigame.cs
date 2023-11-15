9273b35e421ecf86333b31ca2a40949b922bf48a
GODAIGOisME <godaigosine@gmail.com>
2023-11-15 09:52:03 +0900

    11/15

--- 実物/Unity/Goodbye Unity/Assets/Script/minigame.cs
+++ 実物/Unity/Goodbye Unity/Assets/Script/minigame.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class minigame : MonoBehaviour
 {
-    public float detectionRadius = 10f; // プレイヤーを検出する半径
+    public float detectionRadius = 5f; // プレイヤーを検出する半径
     public Color flashColor = Color.white; // 白い画面の色
     public float flashDuration = 1f; // 白い画面が表示される時間
     public GameObject plyerrr;
     private bool isPlayerNear = false;
     public Canvas canvas;
+    public Text naame;
+    public GameObject Mini_gamen;
+    public GameObject Canvas2;
+    public GameObject osutoko;
     void Update()
     {
         // プレイヤーの近くにいるかどうかを検出
         isPlayerNear = IsPlayerNear();
         Debug.Log(isPlayerNear);
         // エンターキーが押されたら白い画面を表示
-        if (isPlayerNear == true)
+        if (isPlayerNear == true && Input.GetKeyDown(KeyCode.I))
         {
-           FlashWhiteScreen();
+            FlashWhiteScreen();
+            Canvas2.SetActive(true);
+
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            osutoko.transform.localPosition = new Vector3(-392, 90, 0);
         }
     }
 
@@ -40,15 +52,6 @@ public class minigame : MonoBehaviour
     void FlashWhiteScreen()
     {
         // 白い画面を表示
-        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-
-        // Image（四角形）を作成してCanvasに追加
-        GameObject squareObject = new GameObject("WhiteSquare");
-        squareObject.transform.SetParent(canvasObject.transform);
-        RectTransform rectTransform = squareObject.AddComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(200, 200); // 四角形のサイズを設定
-
-        Image image = squareObject.AddComponent<Image>();
-        image.color = Color.white; // 白い色を設定
+        Canvas2.SetActive(true);
     }
 }
