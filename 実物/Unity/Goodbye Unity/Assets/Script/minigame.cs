e674bd23f53debf3a1ffe1c5bffa944a43dda45b
GODAIGOisME <godaigosine@gmail.com>
2023-11-20 10:28:39 +0900

    11/20

--- 実物/Unity/Goodbye Unity/Assets/Script/minigame.cs
+++ 実物/Unity/Goodbye Unity/Assets/Script/minigame.cs
@@ -54,7 +54,7 @@ public class minigame : MonoBehaviour
             start_flg = true;
             Canvas2.SetActive(start_flg);
         }
-        else if (Input.GetKeyDown(KeyCode.I))
+        else if (Input.GetKeyDown(KeyCode.I) && nanbonme < 4)
         {
             naame.text = "もうやめるのか？";
             Invoke("Owari", 3.5f);
@@ -75,7 +75,7 @@ public class minigame : MonoBehaviour
                 tugi_flg = true;
                 if (nanbonme == 1)
                 {
-                    if (move_gage.transform.localPosition.x + 20 > gage_rect.sizeDelta.x && move_gage.transform.localPosition.x - 20 < osutoko1.transform.localPosition.x && nanbonme == 1 && tugi_flg == true)
+                    if (move_gage.transform.localPosition.x - 10 <= osutoko1.transform.localPosition.x + 15 && osutoko1.transform.localPosition.x - 15 <= move_gage.transform.localPosition.x + 10 && nanbonme == 1 && tugi_flg == true)
                     {
                         tugi_flg = false;
                         nanbonme = 2;
@@ -98,7 +98,7 @@ public class minigame : MonoBehaviour
                 }
                 if (nanbonme == 2)
                 {
-                    if (move_gage.transform.localPosition.x + 20 > osutoko2.transform.localPosition.x && move_gage.transform.localPosition.x - 20 < osutoko2.transform.localPosition.x && nanbonme == 2 && tugi_flg == true)//2本目
+                    if (move_gage.transform.localPosition.x - 10 <= osutoko2.transform.localPosition.x + 15 && osutoko2.transform.localPosition.x - 15 <= move_gage.transform.localPosition.x + 10 && nanbonme == 2 && tugi_flg == true)//2本目
                     {
                         tugi_flg = false;
                         nanbonme = 3;
@@ -128,7 +128,7 @@ public class minigame : MonoBehaviour
                 }
                 if (nanbonme == 3)
                 {
-                    if (move_gage.transform.localPosition.x + 20 > osutoko3.transform.localPosition.x && move_gage.transform.localPosition.x - 20 < osutoko3.transform.localPosition.x && nanbonme == 3 && tugi_flg == true)//3本目
+                    if (move_gage.transform.localPosition.x - 10 <= osutoko3.transform.localPosition.x + 15 && osutoko3.transform.localPosition.x - 15 <= move_gage.transform.localPosition.x + 10 && nanbonme == 3 && tugi_flg == true)//3本目
                     {
                         tugi_flg = false;
                         nanbonme = 4;
