50e4ba3ecbefbe6c6be7f16821efe0ea32b26453
GODAIGOisME <godaigosine@gmail.com>
2024-01-09 17:16:08 +0900

    1/9

--- 実物/vb_from/時間切れ/TimeUp/Form1.vb
+++ 実物/vb_from/時間切れ/TimeUp/Form1.vb
@@ -1,9 +1,248 @@
-﻿Public Class Form1
+﻿Imports System.Threading
+Public Class Form1
+    Dim Images(2) As Image
+    Dim Label_flg As Boolean = False
+    Dim Songen_flg As Boolean = False
+    Dim Auto_flg As Boolean = False
+    Dim sokudo As Integer = 100
+    Dim v As Integer = 0
+    Dim count2 As Integer = 0
+    Dim count3 As Integer = 0
+    Dim displayText As String = "エンターキーで会話を進められます。" ' 表示する文字列
+    Dim currentIndex As Integer = 0 '
+    Dim ss() As String = {"(俺は制限時間以内に、
+学校から出ることはかなわなかった。)", "(あの後結局脱出するための鍵は
+何とか見つけられた。)", "(しかし腐敗していたのだ。)", "こんな短時間で！？", "そのような疑問が浮かぶが
+その前にお迎えが来た。", "また会ったな。俺ティラノ！！", "ぎゃぁぁぁア！！？？", "熱烈な歓迎でうれしいよ
+Mr.高橋。", "さて、俺がここまで来たのは
+他でもない。", "お前に選択肢を与えに来た！", "何を言ってるんだ
+こいつは…？", "まぁ聞けや。", "お前の選択肢は２つ", "これから俺達に食われるか
+俺たちの専属料理人になるかだ。", "専属料理人だと！？", "あぁ、俺らの朝昼晩の3食…
+あとは、３時におやつを作ってもらう。", "食材はもちろん
+人間だ！！", "カニバリズムには協力できない！", "カニバリズムは人間が人間を
+食うことなんだがな…", "協力しないのなら
+惨たらしく殺す！", "ひえぇぇぇぇェ…", "お前らの食事で例えると
+毎日刺身を食っているようなものなんだ。", "もしお前が毎日食事を作るのなら
+お前の命は保証しよう！", "さぁどうする高橋！？", "料理人になるか死ぬか", "どちらか選べぃ！", "(どうしよう…)", "", "(こうして俺はティラノ共のとこで
+働いた。)", "(「血みどろハンバーグ」や
+肉じゃがの人バージョンの「人じゃが」)", "(人肉ソーセージなどを作る日々だった)", "(やつらはヴィーガン精神の
+かけらもない。)", "(もはや人の解体も無心で行えるように
+なってしまった。)", "(俺の人間性が薄れていく…)", "(もはや心は決壊寸前。)", "(表面張力ぎりぎりまで
+水が入ったコップのようだ。)", "オイッ高橋！
+飯はまだか！", "はいっ！
+すぐにお持ちします！", "もう限界だ！！
+あのクソ・T・Rex がァ！！", "(だが刃物では敵わない。
+この刃渡り15cmの鋭利な刃物で)", "(自ら命を
+絶ってやる！)", "少年は自ら頸動脈と頸静脈に
+ソレを突き刺した！", "アベシッ！！！", "高橋死んだ！", "誰がクソティーレックスだと！
+このガキャー！！", "うわー！
+こいつ死んでやがる！", "こうして高橋は
+人生の終わりを迎えた！", "BAD END! その１
+～高塚はマジ死～", "俺は人間だぞ！", "人間の調理なんかするかよ！", "じゃあ
+食い殺してやるよ！", "やってみろよ
+バァ～カ！！"}
+    Dim ss2() As String = {"[高橋]", "[高橋]", "[高橋]", "[高橋]", "[ナレーション]", "[ティラノサウルス]", "[高橋]", "[ティラノサウルス]", "[ティラノサウルス]", "[ティラノサウルス]", "[高橋]", "[ティラノサウルス]", "[ティラノサウルス]", "[ティラノサウルス]", "[高橋]", "[ティラノサウルス]", "[ティラノサウルス]", "[高橋]", "[ティラノサウルス]", "[ティラノサウルス]", "[高橋]", "[ティラノサウルス]", "[ティラノサウルス]", "[ティラノサウルス]", "[ティラノサウルス]", "[ティラノサウルス]", "[高橋]", " ", "[高橋]", "[高橋]", "[高橋]", "[高橋]", "[高橋]", "[高橋]", "[高橋]", "[高橋]", "[ティラノサウルス]", "[高橋]", "[高橋]", "[高橋]", "[高橋]", "[ナレーション]", "[高橋]", "[ナレーション]", "[ティラノサウルス]", "[ティラノサウルス]", "[ナレーション]", "[ナレーション]", "[高橋]", "[高橋]", "[ティラノサウルス]", "[高橋]"}
+    '料理人は47まで
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
-        PictureBox1.Image = Image.FromFile("新聞.jpg")
+        Label13.Visible = False
+        Label14.Visible = False
+        'PictureBox1.Image = Image.FromFile("新聞.jpg")
+        Images(0) = Image.FromFile("学校の中.png")
+        Images(1) = Image.FromFile("black.png")
+        Timer1.Interval = 10
+        Timer2.Interval = 100
+        Timer1.Enabled = True
+        Timer2.Enabled = True
+        displayText = ss(count2)
+        Label7.Text = ss2(count2)
+        Label1.Text = ""
+        Label6.Text = "▶1倍速"
+        Label4.Text = "------------"
+        Label3.Text = "------------"
+        Label2.Text = "------------"
+        Label8.Text = "------------"
+        Label9.Text = "------------"
+        Label10.Text = "------------"
+        AddHandler Me.MouseWheel, AddressOf Form1_MouseWheel
+    End Sub
+    Private Sub Mojiyomi(h As Integer)
+        If currentIndex < displayText.Length Then
+            Thread.Sleep(h)
+            Label1.Text &= displayText(currentIndex) ' 現在の位置の文字を追加
+            currentIndex += 1
+        End If
+
+    End Sub
+    Private Sub Tugi()
+        Label1.Text = ""
+        currentIndex = 0
+        count2 += 1
+        displayText = ss(count2)
+            Label7.Text = ss2(count2)
+            count3 = count2
+            v = count3 - 3
+        Serekuto()
+    End Sub
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Enter And Label_flg = False And Auto_flg = False And Not count2 = 27 And Not count2 = 47 Then
+            Tugi()
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Mojiyomi(sokudo)
+        Label12.Text = count2
+        Select Case count2
+            Case <= 4
+                PictureBox1.Image = Images(0)
+            Case 27
+                Label11.ForeColor = Color.White
+                Timer1.Stop()
+                Auto_flg = False
+                'Timer2.Stop()
+                Label13.Visible = True
+                Label14.Visible = True
+            Case 47
+                Auto_flg = False
+                Label15.Visible = True
+        End Select
+
+
     End Sub
 
 
 
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+        If Label_flg = False Then
+            PictureBox3.Visible = True
+            Label4.Visible = True
+            Label3.Visible = True
+            Label2.Visible = True
+            Label8.Visible = True
+            Label9.Visible = True
+            Label10.Visible = True
+            Label_flg = True
+            Label5.ForeColor = Color.Red
+        Else
+            Label5.ForeColor = Color.White
+            PictureBox3.Visible = False
+            Label_flg = False
+            Label4.Visible = False
+            Label3.Visible = False
+            Label2.Visible = False
+            Label8.Visible = False
+            Label9.Visible = False
+            Label10.Visible = False
+        End If
 
+
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+        Select Case sokudo
+            Case 100
+                sokudo = 50
+                Label6.Text = "▶▶2倍速"
+            Case 50
+                sokudo = 200
+                Label6.Text = "0.5倍速"
+            Case 200
+                sokudo = 100
+                Label6.Text = "▶1倍速"
+        End Select
+
+
+    End Sub
+    Private Sub Serekuto()
+        Select Case v
+            Case -3
+                Label4.Text = "------------"
+                Label3.Text = "------------"
+                Label2.Text = "------------"
+                Label8.Text = "------------"
+                Label9.Text = "------------"
+                Label10.Text = "------------"
+            Case -2
+                Label4.Text = "------------"
+                Label3.Text = "------------"
+                Label2.Text = ss(count3 - 1)
+                Label8.Text = "------------"
+                Label9.Text = "------------"
+                Label10.Text = ss2(count3 - 1)
+            Case -1
+                Label4.Text = "------------"
+                Label3.Text = ss(count3 - 2)
+                Label2.Text = ss(count3 - 1)
+                Label8.Text = "------------"
+                Label9.Text = ss2(count3 - 2)
+                Label10.Text = ss2(count3 - 1)
+            Case >= 0
+                Label4.Text = ss(count3 - 3)
+                    Label3.Text = ss(count3 - 2)
+                    Label2.Text = ss(count3 - 1)
+                    Label8.Text = ss2(count3 - 3)
+                    Label9.Text = ss2(count3 - 2)
+                Label10.Text = ss2(count3 - 1)
+        End Select
+    End Sub
+    Private Sub Form1_MouseWheel(sender As Object, e As MouseEventArgs)
+        If e.Delta > 0 And count3 - 4 >= 0 Then
+            count3 -= 1
+            Serekuto()
+        ElseIf e.Delta <= 0 And count3 < count2 Then
+            count3 += 1
+            Serekuto()
+        End If
+    End Sub
+
+    Private Sub Label11_Click(sender As Object, e As EventArgs) Handles Label11.Click
+        If count2 = 27 Or count2 = 45 Then
+            Label11.ForeColor = Color.White
+            Auto_flg = False
+        Else
+            If Auto_flg = False Then
+                Label11.ForeColor = Color.Red
+                Auto_flg = True
+            ElseIf Auto_flg = True Then
+                Label11.ForeColor = Color.White
+                Auto_flg = False
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If Auto_flg = True And currentIndex >= displayText.Length Then
+            Thread.Sleep(800)
+            Tugi()
+        End If
+    End Sub
+
+    Private Sub Label14_Click(sender As Object, e As EventArgs) Handles Label14.Click
+        'Songen_flg = True
+        Timer1.Start()
+        count2 = 48
+        count3 = 48
+        Tugi()
+        ss(count3 - 1) = "-> 人としての尊厳を守る"
+        ss2(count3 - 1) = "[高橋]"
+        Label14.Visible = False
+        Label13.Visible = False
+    End Sub
+
+    Private Sub Label13_Click(sender As Object, e As EventArgs) Handles Label13.Click
+        Timer1.Start()
+        PictureBox1.Image = Images(1)
+        Tugi()
+        ss(count3 - 1) = "-> 専属料理人になる"
+        ss2(count3 - 1) = "[高橋]"
+        Label14.Visible = False
+        Label13.Visible = False
+    End Sub
+
+    Private Sub Label12_Click(sender As Object, e As EventArgs) Handles Label12.Click
+
+    End Sub
 End Class
+
